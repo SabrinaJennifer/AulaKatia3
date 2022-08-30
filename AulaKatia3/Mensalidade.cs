a3e5116433ae90c9f549a34fdf5eecb2b2de61c3
curso.ads <curso.ads@LAB06-22>
2022-08-29 21:02:14 -0300

    Adicionar arquivos de projeto.

---
+++ AulaKatia3/Mensalidade.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaKatia3
+{
+    internal class Mensalidade
+    {
+        string curso;
+        double valor;
+        int dia;
+
+
+        public void SetCurso(string curso)
+        {
+            this.curso = curso;
+        }
+        public string GetCurso()
+        {
+            return this.curso;
+        }
+        public void SetValor(double valor)
+        {
+            this.valor = valor;
+        }
+        public double GetValor()
+        {
+            return this.valor;
+
+        }
+        public void SetDia(int dia)
+        {
+            this.dia = dia;
+        }
+        public int GetDia()
+        {
+            return this.dia;
+        }
+       public  double Calcular()
+        {
+            if(this.dia<= 5)
+            {
+                return this.valor * 0.95;
+            }
+            else{
+
+                if (this.dia > 10)
+                {
+                    return this.valor * 1.02;
+                }
+                else
+                {
+                    return this.valor;
+                }
+            }
+        }
+
+    }
+}
---
+++ AulaKatia3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaKatia3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaração em instanciação dos objetos
+            Aluno al= new Aluno();
+            Mensalidade men = new Mensalidade();
+            //chamada dos métodos
+            Console.Write("Digite o nome: ");
+            al.SetNome(Console.ReadLine());
+            Console.Write("Digite o endereço: ");
+            al.SetEndereco(Console.ReadLine());
+            Console.Write("Digite o Cpf: ");
+            al.SetCpf(Console.ReadLine());
+            Console.Write("Digite o RA: ");
+            al.SetRa(Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Digite o curso: ");
+            men.SetCurso(Console.ReadLine());
+            Console.Write("Digite o vaçor: ");
+            men.SetValor(Convert.ToDouble(Console.ReadLine()));
+            Console.Write("Digite o dia do pagamento: ");
+            men.SetDia(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("\nDados do Aluno");
+            Console.WriteLine("RA: " + al.GetRa() + "\nAluno: " + al.GetNome());
+            Console.WriteLine("Endereço: " + al.GetEndereco() + "\nCPF: " + al.GetCpf());
+            Console.WriteLine("Cuso: " +men.GetCurso() + "\nValor da Mensalidade: " + men.GetValor().ToString("C")+"\nValor a pagar: "+men.Calcular().ToString("C"));
+
+        }
+    }
+}
